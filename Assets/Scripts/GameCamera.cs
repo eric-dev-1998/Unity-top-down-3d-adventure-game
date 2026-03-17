c01b9dcb17d7c959f8e85d7c7d395f78b6f638b2
eric-dev-1998 <ericbusiness103@outlook.com>
2026-03-17 11:35:04 -0500

    chore: Adjuested camera proximity senitivity to delay it a little on the set_camera_target event

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -39,7 +39,7 @@ public class GameCamera : MonoBehaviour
         Vector3 targetPos = new Vector3(focus_target.position.x + offset.x, focus_target.position.y + offset.y, focus_target.position.z - targetZOffset);
         transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * followSpeed);
 
-        if (Vector3.Distance(transform.position, targetPos) <= 0.25f)
+        if (Vector3.Distance(transform.position, targetPos) <= 0.06f)
             inPosition = true;
         else
             inPosition = false;
