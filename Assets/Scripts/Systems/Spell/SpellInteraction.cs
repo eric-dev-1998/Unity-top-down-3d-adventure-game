87652fc858ae42be798a939a5d8bd7da1914e15f
eric-dev-1998 <ericbusiness103@outlook.com>
2026-04-03 10:20:08 -0500

    feat: Created a basic spell interaction code.

---
+++ Assets/Scripts/Systems/Spell/SpellInteraction.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+namespace Assets.Scripts.Systems.Spell
+{
+    public class SpellInteraction : MonoBehaviour
+    {
+        private float durationInSeconds = 5f;
+        private float cooldownInSeconds = 1f;
+
+        private bool reacting = false;
+
+        public void React(string tag)
+        {
+            switch (tag)
+            {
+                case "Spell_Neutral":
+                    break;
+
+                case "Spell_Fire":
+                    Burn();
+                    break;
+
+                case "Spell_Water":
+                    Soak();
+                    break;
+
+                case "Spell_Wind":
+                    break;
+
+                case "Spell_Earth":
+                    break;
+            }
+        }
+
+        private void Burn()
+        {
+            if (reacting)
+                return;
+
+            GameObject vfxAsset = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Spells/BurningParticles.prefab");
+            GameObject vfx = Instantiate(vfxAsset, transform);
+
+            ParticleSystem particles = vfx.GetComponent<ParticleSystem>();
+            ShapeModule shapeModule = particles.shape;
+
+            shapeModule.shapeType = ParticleSystemShapeType.Mesh;
+            shapeModule.mesh = GetComponent<MeshFilter>().mesh;
+            shapeModule.scale = transform.localScale;
+
+            reacting = true;
+        }
+
+        private void Soak()
+        {
+            if (reacting)
+                return;
+
+            GameObject vfxAsset = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Spells/WetParticles.prefab");
+            GameObject vfx = Instantiate(vfxAsset, transform);
+
+            ParticleSystem particles = vfx.GetComponent<ParticleSystem>();
+            ShapeModule shapeModule = particles.shape;
+
+            shapeModule.shapeType = ParticleSystemShapeType.Mesh;
+            shapeModule.mesh = GetComponent<MeshFilter>().mesh;
+            shapeModule.scale = transform.localScale;
+
+            reacting = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            React(other.tag);
+        }
+    }
+}
