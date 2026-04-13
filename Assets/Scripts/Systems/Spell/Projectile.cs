464358629b256601847fcd7e254660e92da18293
eric-dev-1998 <ericbusiness103@outlook.com>
2026-04-13 09:55:32 -0500

    feat: Added two extra fields: caster & power

--- Assets/Scripts/Systems/Spell/Projectile.cs
+++ Assets/Scripts/Systems/Spell/Projectile.cs
@@ -7,9 +7,11 @@ namespace Assets.Scripts.Systems.Spell
 {
     public class Projectile : MonoBehaviour
     {
+        public int power = 1;
         public float speed = 3f;
         public float timeToDisappear = 5f;
         private float timeElapsed = 0f;
+        private SpellCaster caster;
 
         private void Update()
         {
@@ -25,6 +27,15 @@ namespace Assets.Scripts.Systems.Spell
             Destroy(gameObject);
         }
 
+        public void SetCaster(SpellCaster caster)
+        { 
+            this.caster = caster;
+        }
+
+        public SpellCaster GetCaster()
+        { 
+            return caster;
+        }
         private void OnCollisionEnter(Collision collision)
         {
             DestroySelf();
