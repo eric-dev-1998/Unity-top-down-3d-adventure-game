57796f9ffd91f28689ba96a56682552892a6da2f
eric-dev-1998 <ericbusiness103@outlook.com>
2026-04-22 10:27:04 -0500

    feat: Coded logic to update the magic selector icon when the player switch powers by scrolling with the mouse.

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -1,18 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
+using Assets.Scripts.Player;
 using UnityEngine;
+using UnityEngine.UIElements;
+using System.Collections.Generic;
+using System.Linq;
 
-public class HUDManager : MonoBehaviour
+namespace Assets.Scripts
 {
-    // Coin and keys counter text object:
-    public Text coinCounter;
-    public Text keyCounter;
-
-    // Update is called once per frame
-    void Update()
+    public class HUDManager : MonoBehaviour
     {
-        coinCounter.text = "Monedas: " + PlayerInventory.coins.ToString();
-        keyCounter.text = "Llaves" + PlayerInventory.keys.ToString();
+        private VisualElement healthBar;
+        private VisualElement magicBar;
+        private VisualElement magicIcon;
+
+        private List<Texture2D> magicIcons;
+
+        private UIDocument document;
+
+        private PlayerCore _player;
+
+        private void Start()
+        {
+            if (!LoadMagicIcons())
+                Debug.Log("Unable to load magic icons.");
+
+            document = GetComponent<UIDocument>();
+            _player = FindAnyObjectByType<PlayerCore>();
+
+            healthBar = document.rootVisualElement.Q<VisualElement>("PlayerHealth").Q<VisualElement>("Bar");
+            magicBar = document.rootVisualElement.Q<VisualElement>("PlayerMana").Q<VisualElement>("Bar");
+            magicIcon = document.rootVisualElement.Q<VisualElement>("MagicSelector").Q<VisualElement>("Icon");
+        }
+
+        private void Update()
+        {
+            int mouseScrollDelta = Mathf.CeilToInt(Input.mouseScrollDelta.y);
+            if (mouseScrollDelta != 0)
+                SelectMagic(mouseScrollDelta);
+        }
+
+        private bool LoadMagicIcons()
+        {
+            magicIcons = Resources.LoadAll<Texture2D>("Art/Icons/").ToList();
+            if(magicIcons.Count != 0)
+                return true;
+            else 
+                return false;   
+        }
+
+        public void SetHealth(int value)
+        { 
+            
+        }
+
+        public void SetMagic(int value) 
+        {
+            magicBar.style.width = (int) (472f * (value / 100f));
+        }
+
+        private void SelectMagic(int i)
+        {
+            Debug.Log(i);
+
+            _player.SelectMagic(i);
+            ChangeSelectedMagicIcon();
+        }
+
+        private void ChangeSelectedMagicIcon()
+        {
+            float index = _player.GetSelectedMagicIndex();
+            if(index >= 0)
+                magicIcon.style.backgroundImage = magicIcons.Find(icon => icon.name == _player.GetSelectedMagicName());
+        }
     }
 }
