add525898d71264a211f157669595cc21969ac99
eric-dev-1998 <ericbusiness103@outlook.com>
2026-04-22 10:39:02 -0500

    feat: Coded a method to switch between already unlocked magic powers at runtime.

--- Assets/Scripts/Player/PlayerCore.cs
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -1,3 +1,8 @@
+using Assets.Scripts.Inventory_System;
+using Assets.Scripts.Systems.Spell;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Player
@@ -12,7 +17,11 @@ namespace Assets.Scripts.Player
         public bool onGrab = false;
         public float currentWaterHeight = 0f;
 
+        private Dictionary<int, string> _availableMagic = new();
+
         private float velocity = 0;
+        private int _selectedMagicIndex = -1;
+        private int _selectedMagicKey = -1;
 
         private void Awake()
         {
@@ -59,6 +68,74 @@ namespace Assets.Scripts.Player
         {
             canMove = true;
             entity.UnlockMovement();
+        }
+
+        public void UnlockMagicPower(int index)
+        {
+            if (_availableMagic.ContainsKey(index))
+                return;
+
+            switch (index)
+            {
+                case 0:
+                    // Neutral
+                    _availableMagic.Add(index, "Neutral");
+                    break;
+
+                case 1:
+                    // Fire
+                    _availableMagic.Add(index, "Fire");
+                    break;
+
+                case 2:
+                    // Water
+                    _availableMagic.Add(index, "Water");
+                    break;
+
+                case 3:
+                    // Neutral
+                    _availableMagic.Add(index, "Wind");
+                    break;
+
+                case 4:
+                    // Neutral
+                    _availableMagic.Add(index, "Earth");
+                    break;
+            }
+        }
+
+
+        public void SelectMagic(int i)
+        {
+            int newIndex = _selectedMagicIndex + i;
+
+            // Clamp index value.
+            if (newIndex > _availableMagic.Count - 1)
+                newIndex = 0;
+
+            if (newIndex < 0)
+                newIndex = _availableMagic.Count - 1;
+
+            // Select magic.
+            if (_availableMagic.Count > 0)
+            {
+                var magic = _availableMagic.ElementAt(newIndex);
+                _selectedMagicIndex = newIndex;
+                _selectedMagicKey = magic.Key;
+
+                SpellCaster caster = GetComponent<SpellCaster>();
+                caster.SetElement(_selectedMagicKey);
+            }
+        }
+
+        public int GetSelectedMagicIndex()
+        {
+            return _selectedMagicKey;
+        }
+
+        public string GetSelectedMagicName()
+        {
+            return _availableMagic[_selectedMagicKey];
         }
     }
 }
