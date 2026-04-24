7c220059234d0c6df8a6c45e0d06c4f4fd70ac5f
eric-dev-1998 <ericbusiness103@outlook.com>
2026-04-23 19:26:14 -0500

    feat: Created the first class to handle game data serialization.

---
+++ Assets/Scripts/GameSerialization/PlayerInventory.cs
@@ -0,0 +1,10 @@
+namespace Assets.Scripts.GameSerialization
+{
+    [System.Serializable]
+    static class PlayerInventory
+    {
+        public static int PowerOrbs = 0;
+        public static int MagicCrystals = 0;
+        public static int Spirits = 0;
+    }
+}
