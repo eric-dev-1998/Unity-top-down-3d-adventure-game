7decfde029ca885e3f893a05981495a19c1b1dca
eric-dev-1998 <ericbusiness103@outlook.com>
2026-04-24 19:38:43 -0500

    feat: Created the class for collectibles on scene.

--- Assets/Scripts/World/Collectible.cs
+++ Assets/Scripts/World/Collectible.cs
@@ -1,36 +1,58 @@
 using Assets.Scripts.GameSerialization;
 using Assets.Scripts.Player;
+using Assets.Scripts.Systems.Spell;
 using UnityEngine;
 
 namespace Assets.Scripts.World
 {
     public class Collectible : MonoBehaviour
     {
-        public enum CollectibleType { Life_Stone, Magic_Crystal, Spirit, Power_Orb };
+        public enum CollectibleType { None, Life_Stone, Magic_Crystal, Spirit, Power_Orb };
         public CollectibleType Type;
 
         private void CollectLifeStone()
         {
-            PlayerCore player = FindAnyObjectByType<PlayerCore>();
-            player.GetEntity().health += 1;
+            Entity player = FindAnyObjectByType<PlayerCore>().GetEntity();
+            player.AddHealth(1);
+
+            HUDManager hudManager = FindAnyObjectByType<HUDManager>();
+            hudManager.SetHealth(player.health, player.maxHealth);
+
+            Destroy(gameObject);
         }
 
         private void CollectMagicCrystal()
         {
+            SpellCaster player = FindAnyObjectByType<PlayerCore>().GetComponent<SpellCaster>();
+            player.AddMana(2);
+
+            HUDManager hudManager = FindAnyObjectByType<HUDManager>();
+            hudManager.SetMagic(player.mana, player.maxMana);
+
             PlayerInventory.MagicCrystals += 1;
-            Destroy(this);
+
+            hudManager.DisplayCollected(Type);
+            Destroy(gameObject);
         }
 
         private void CollectSpirit()
         {
             PlayerInventory.Spirits += 1;
-            Destroy(this);
+
+            HUDManager hudManager = FindAnyObjectByType<HUDManager>();
+            hudManager.DisplayCollected(Type);
+
+            Destroy(gameObject);
         }
 
         private void CollectPowerOrb()
         { 
             PlayerInventory.PowerOrbs += 1;
-            Destroy(this);
+
+            HUDManager hudManager = FindAnyObjectByType<HUDManager>();
+            hudManager.DisplayCollected(Type);
+
+            Destroy(gameObject);
         }
 
         private void OnTriggerEnter(Collider other)
@@ -40,15 +62,19 @@ namespace Assets.Scripts.World
                 switch (Type) 
                 {
                     case CollectibleType.Life_Stone:
+                        CollectLifeStone();
                         break;
 
                     case CollectibleType.Magic_Crystal:
+                        CollectMagicCrystal();
                         break;
 
                     case CollectibleType.Spirit:
+                        CollectSpirit();
                         break;
 
                     case CollectibleType.Power_Orb:
+                        CollectPowerOrb();
                         break;
                 }
             }
