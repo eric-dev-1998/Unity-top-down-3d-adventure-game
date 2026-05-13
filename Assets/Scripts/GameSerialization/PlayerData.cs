07dd7f96083394835cd2c5a45cac194868d23d92
eric-dev-1998 <ericbusiness103@outlook.com>
2026-05-13 17:36:51 -0500

    feat: Renamed the file to PlayerData and added fields for collectables count.

---
+++ Assets/Scripts/GameSerialization/PlayerData.cs
@@ -0,0 +1,15 @@
+namespace Assets.Scripts.GameSerialization
+{
+    [System.Serializable]
+    static class PlayerData
+    {
+        public static int PowerOrbs = 0;
+        public static int MagicCrystals = 0;
+        public static int Spirits = 0;
+
+        public static bool FirstLifeStone = true;
+        public static bool FirstMagicCrystal = true;
+        public static bool FirstPowerOrb = true;
+        public static bool FirstSpirit = true;
+    }
+}
