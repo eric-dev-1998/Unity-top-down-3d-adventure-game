a22e05f2d8320c8024dafdc5f374f48fa8815c71
eric-dev-1998 <ericbusiness103@outlook.com>
2026-05-13 18:10:08 -0500

    feat: Created monobehavior scripts for gameover and screenvfx ui documents.

---
+++ Assets/Scripts/GameMenu/UXML_ScreenVFX.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.GameMenu
+{
+    public class UXML_ScreenVFX : MonoBehaviour
+    {
+        public enum AwakeFadeAnimation { DarkOut, LightOut, None }
+        public AwakeFadeAnimation awakeFadeAnimation;
+
+        private VisualElement _veil;
+
+        private UIDocument _document;
+
+        void Start()
+        {
+            _document = GetComponent<UIDocument>();
+            _veil = _document.rootVisualElement.Q("Veil");
+
+            StartCoroutine(PlayAwakeFadeAnimation());
+        }
+
+        public IEnumerator PlayAwakeFadeAnimation()
+        {
+            var originalDuration = _veil.style.transitionDuration;
+
+            switch (awakeFadeAnimation)
+            {
+                case AwakeFadeAnimation.DarkOut:
+                    _veil.style.transitionDuration = new List<TimeValue> { new TimeValue(0, TimeUnit.Second) };
+                    FadeToDark();
+
+                    yield return null;
+
+                    _veil.style.transitionDuration = originalDuration;
+                    Clear();
+
+                    yield break;
+
+                case AwakeFadeAnimation.LightOut:
+                    _veil.style.transitionDuration = new List<TimeValue> { new TimeValue(0, TimeUnit.Second) };
+                    FadeToLight();
+
+                    yield return null;
+
+                    _veil.style.transitionDuration = originalDuration;
+                    Clear();
+                    yield break;
+
+                default:
+                    yield break;
+            }
+        }
+
+        public void FadeToDark() 
+        {
+            _veil.RemoveFromClassList("screen-fade-light");
+            _veil.AddToClassList("screen-fade-dark");
+        }
+
+        public void FadeToLight() 
+        {
+            _veil.RemoveFromClassList("screen-fade-dark");
+            _veil.AddToClassList("screen-fade-light");
+        }
+
+        public void Clear() 
+        {
+            _veil.RemoveFromClassList("screen-fade-light");
+            _veil.RemoveFromClassList("screen-fade-dark");
+        }
+
+        public VisualElement GetScreenVeil()
+        {
+            return _veil;
+        }
+    }
+}
---
+++ Assets/Scripts/GameMenu/Uxml_GameOver.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace Assets.Scripts.GameMenu
+{
+    public class Uxml_GameOver : MonoBehaviour
+    {
+        private Label _text;
+        private Button _button;
+        private VisualElement _background;
+
+        private UIDocument _document;
+
+        private void Start()
+        {
+            _document = GetComponent<UIDocument>();
+
+            _text = _document.rootVisualElement.Q<Label>("Text");
+            _background = _document.rootVisualElement.Q("Background");
+
+            _button = _document.rootVisualElement.Q<Button>("Button_Retry");
+            _button.clicked += Retry;
+        }
+
+        public void Show()
+        {
+            // 1. Show darker background.
+            _background.AddToClassList("t-darken");
+
+            // 2. Show text and buttons.
+            _text.AddToClassList("t-vslide-to-center");
+            _button.AddToClassList("t-vslide-to-center");
+        }
+
+        private void Retry()
+        {
+            _button.SetEnabled(false);
+            StartCoroutine(ReloadScene());
+        }
+
+        private IEnumerator ReloadScene()
+        {
+            // 1. Darken screen:
+            var screenvfx = FindAnyObjectByType<UXML_ScreenVFX>();
+            screenvfx.FadeToDark();
+
+            StyleColor darkColor = new StyleColor();
+            darkColor.value = new Color(0, 0, 0, 1);
+
+            yield return new WaitUntil(() => screenvfx.GetScreenVeil().resolvedStyle.backgroundColor == darkColor);
+
+            yield return new WaitForSeconds(1f);
+            SceneManager.LoadScene("Demo");
+        }
+    }
+}
